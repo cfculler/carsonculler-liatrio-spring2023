3fe683be04974464214e0110c9cbb5b2253b6230
cfculler <99631280+cfculler@users.noreply.github.com>
2023-04-16 13:29:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('json spaces', "\t");
 
 app.get('/', (request, response) => {
     response.json({
-        message: "My name is Carson Culler",
+        message: "My name is Carson",
         timestamp: Date.now(),
     })
 })
